212c9d7f99f7bc26e9511b27dd7bfa29636b1248
Lucas <lucaslukowski@gmail.com>
2019-01-14 11:22:43 -0700

    removed EndPoint.get

--- lib/src/models/endpoint.dart
+++ lib/src/models/endpoint.dart
@@ -1,6 +1,4 @@
 abstract class EndPoint {
   String endpoint;
   Map<String, dynamic> params;
-
-  Uri get(Uri url);
 }
--- lib/src/models/route.dart
+++ lib/src/models/route.dart
@@ -4,16 +4,8 @@ class Route implements EndPoint {
   var endpoint;
   var params;
 
-  Route(String endpoint, [Map<String, dynamic> params]) {
+  Route(String endpoint, [Map<String, dynamic> payload]) {
     this.endpoint = endpoint;
-    this.params = params ?? {};
-  }
-
-  @override
-  Uri get(Uri url) {
-    var ep = Uri.parse(url.toString() + endpoint);
-    params.addAll(ep.queryParameters);
-    ep.replace(queryParameters: params);
-    return ep;
+    this.params = payload ?? {};
   }
 }
