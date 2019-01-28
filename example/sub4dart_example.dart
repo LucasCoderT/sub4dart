929f212180c0589ac6daf6218bf343c3382bfc10
Lucas <lucaslukowski@gmail.com>
2019-01-28 09:49:11 -0700

    Updated example

--- example/sub4dart_example.dart
+++ example/sub4dart_example.dart
@@ -1,6 +1,8 @@
+import 'package:sub4dart/src/models/subsonic_response.dart';
 import 'package:sub4dart/sub4dart.dart';
 
-main() {
+main() async {
   var subsonic = new SubSonic("https://music.example.com", "John", "Doe");
-
+  SubSonicResponse response = await subsonic.getPing();
+  print(response.isOkay);
 }
